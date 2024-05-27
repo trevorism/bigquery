ec0e4c5623791a87bfd1c3a7815ce95d6ea62cc4
Trevor Brooks <trevorism@gmail.com>
2024-05-27 10:32:18 -0400

    unit test

---
+++ src/test/groovy/com/trevorism/service/InMemoryDescribeServiceTest.groovy
@@ -0,0 +1,19 @@
+package com.trevorism.service
+
+import com.trevorism.model.Describe
+import org.junit.jupiter.api.Test
+
+class InMemoryDescribeServiceTest {
+
+    @Test
+    void testDescribe(){
+        DescribeService describeService = new InMemoryDescribeService()
+        Describe describe = new Describe()
+        List<String> result = describeService.describe(describe)
+        assert result
+        assert result.size() == 3
+        assert result.contains("list")
+        assert result.contains("create")
+        assert result.contains("read")
+    }
+}
